1ae59c29558b708b72b87e446633263efdbca3d3
Raymonddv <raymond_max@hotmail.com>
2017-12-13 16:02:09 +0100

    s

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user', function () {
+Route::get('/users', function () {
     return UserResource::collection(User::all());
 });

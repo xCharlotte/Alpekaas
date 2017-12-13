375ac8461f1a123456ac8eaa9483b2d6d82c6f74
Raymonddv <raymond_max@hotmail.com>
2017-12-13 15:51:31 +0100

    User json api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Http\Resources\User as UserResource;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +16,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/user', function () {
+    return UserResource::collection(User::all());
 });

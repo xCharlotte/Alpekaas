47e2098dd3385519490f2cbb472a4e319d1f68fa
xCharlotte <charlottevoskuilen@hotmail.com>
2017-12-14 14:04:50 +0100

    Setup the routes for the index of safetycheck

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/safetycheck', 'SafetyValueController@index')->name('safetycheck');
 
 // admin prefix
 Route::prefix('admin')->group(function() {

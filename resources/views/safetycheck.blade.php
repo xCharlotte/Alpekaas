df5d51520c2e37225e231189bd8416a677c8f88f
xCharlotte <charlottevoskuilen@hotmail.com>
2017-12-18 15:20:04 +0100

    Must be something about vue

--- resources/views/safetycheck.blade.php
+++ resources/views/safetycheck.blade.php
@@ -2,23 +2,46 @@
 @extends('layouts.app')
 
 @section('content')
+  <script src="https://unpkg.com/vue@2.5.10/dist/vue.js"></script>
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Safetycheck</div>
                 <div class="panel-body">
-                @foreach($anonymous as $noname)
-                  <button type="button" value="{{$noname}}" name="sad"></button>
-                @endforeach
 
-                @foreach($showName as $name)
-                  <button type="button" value="{{$name}}" name="happy"></button>
-                @endforeach
+                <div id="returnvalue">
 
-                    @if (count($anonymous) <= '2')
+                  @foreach($anonymous as $noname)
+                    <button v-on:click.native="sadClick()"></button>
+                  @endforeach
+
+                  @foreach($showName as $name)
+                    <button v-on:click.native="sadClick()"> </button>
+                  @endforeach
+
+                </div>
+                <script>
+                  new Vue({
+                    el: '#returnvalue',
+                    module.exports = {
+                    data: function(){
+                      return {
+                      noname: 1
+                      }
+                    },
+                    methods: {
+                      sadClick: function() {
+                         this.noname += 1;
+                      }
+                    }
+                  };
+                </script>
+
+                    @if (count($noname) <= '2')
                       <p>No email</p>
-                    @elseif (count($showName) >= '3')
+                    @elseif (count($name) >= '3')
                       <p>Yes email!</p>
                     @else
                       <p>Okay</p>
